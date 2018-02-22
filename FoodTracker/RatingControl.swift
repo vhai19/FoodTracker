fdfcb4d671ae3f6fc9e11fc1bed77f2cf5d03ef2
Nguyen Van Hai <n.vanhai19@gmail.com>
2018-02-22 22:49:29 +0900

    [FoodTracker] Display the custom view

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,29 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Nguyen Van Hai on 2018/02/22.
+//  Copyright © 2018 Carmate. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    // MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
