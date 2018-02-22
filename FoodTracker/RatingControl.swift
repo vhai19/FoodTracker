c51d9930adba746eada9fba55648b4d996c8d691
Nguyen Van Hai <n.vanhai19@gmail.com>
2018-02-22 23:28:42 +0900

    [FoodTracker] Add a button to view and build-in an action

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -13,10 +13,35 @@ class RatingControl: UIStackView {
     // MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+    
+    // MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    // MARK: Private Methods
+    private func setupButtons() {
+        
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add contraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
     }
     /*
     // Only override draw() if you perform custom drawing.
