75146d5dfcc871cc7e70e35f54c6aaba53d7b45a
Nguyen Van Hai <n.vanhai19@gmail.com>
2018-02-25 11:03:36 +0900

    [FoodTracker] Add five buttons and add support for Interface Builder

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
+    
+    // MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
 
     // MARK: Initialization
     override init(frame: CGRect) {
@@ -27,21 +33,25 @@ class RatingControl: UIStackView {
     }
     // MARK: Private Methods
     private func setupButtons() {
-        
-        // Create the button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
-        
-        // Add contraints
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        // Setup the button action
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
-        
-        // Add the button to the stack
-        addArrangedSubview(button)
+        for _ in 0..<5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // Add contraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+        }
     }
     /*
     // Only override draw() if you perform custom drawing.
