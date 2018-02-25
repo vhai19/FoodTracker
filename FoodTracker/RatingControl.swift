d80e615bad23f7777a026b2a0c96f3a2ffd96e41
Nguyen Van Hai <n.vanhai19@gmail.com>
2018-02-25 11:32:54 +0900

    [FoodTracker] Add images to the button

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -12,9 +12,17 @@ import UIKit
     
     // MARK: Properties
     private var ratingButtons = [UIButton]()
-    
     var rating = 0
-    
+    @IBInspectable var startSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var startCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
 
     // MARK: Initialization
     override init(frame: CGRect) {
@@ -33,15 +41,34 @@ import UIKit
     }
     // MARK: Private Methods
     private func setupButtons() {
-        for _ in 0..<5 {
+        
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        // Load button images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
+        for _ in 0..<startCount {
             // Create the button
             let button = UIButton()
-            button.backgroundColor = UIColor.red
+//            button.backgroundColor = UIColor.red
+            // Set the button images
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
             
             // Add contraints
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: startSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: startSize.width).isActive = true
             
             // Setup the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
