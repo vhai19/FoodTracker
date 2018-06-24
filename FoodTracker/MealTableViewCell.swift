58232a150aee9e3c6db573c39d1f97cadce980d6
Nguyen Van Hai <n.vanhai19@gmail.com>
2018-06-24 17:11:12 +0900

    [FoodTracker] Create Table View and design custom Table Cells

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Nguyen Van Hai on 2018/06/24.
+//  Copyright © 2018 Carmate. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
